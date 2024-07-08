53200272af2daa7f91090359c53a29be074edca2
Tofaiel-1 <158508049+Tofaiel-1@users.noreply.github.com>
2024-07-08 22:09:21 +0600

    goto function

---
+++ goto Statement/c++ goto.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using  namespace std;
+int main()
+{
+
+ineligible:
+    cout << "You are not eligible to vote!\n";
+    cout << "Enter your age :";
+  int age;
+  cin >> age;
+  if (age  < 18)
+      goto ineligible;
+
+   else
+  {
+cout << "You are eligible to vote";
+  }
+return 0;
+  }
+
